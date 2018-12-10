fb8d941e1077d836b8f48b854811d4caf87713ab
zhubo <zhubo@zhiwyl.com>
2018-12-10 11:08:51 +0800

    上传图片时加入压缩功能

--- app/src/main/java/com/expo/contract/presenter/FindPublishPresenterImpl.java
+++ app/src/main/java/com/expo/contract/presenter/FindPublishPresenterImpl.java
@@ -1,15 +1,13 @@
 package com.expo.contract.presenter;
 
-import android.location.Location;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.blankj.utilcode.util.StringUtils;
+import com.blankj.utilcode.util.TimeUtils;
 import com.expo.base.ExpoApp;
+import com.expo.base.utils.FileUtils;
 import com.expo.contract.FindPublishContract;
-import com.expo.contract.SeekHelpContract;
 import com.expo.entity.Find;
-import com.expo.entity.VisitorService;
 import com.expo.network.Http;
 import com.expo.network.ResponseCallback;
 import com.expo.network.response.BaseResponse;
@@ -17,20 +15,16 @@ import com.expo.network.response.UploadRsp;
 import com.expo.utils.Constants;
 
 import java.io.File;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
-import io.reactivex.Flowable;
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import top.zibin.luban.CompressionPredicate;
 import top.zibin.luban.Luban;
 import top.zibin.luban.OnCompressListener;
+import top.zibin.luban.OnRenameListener;
 
 public class FindPublishPresenterImpl extends FindPublishContract.Presenter {
     public FindPublishPresenterImpl(FindPublishContract.View view) {
@@ -46,94 +40,106 @@ public class FindPublishPresenterImpl extends FindPublishContract.Presenter {
     }
 
     public void upLoadImgFile(Find find, String filePath, int positon) {
-//        if (!StringUtils.isEmpty(filePath)) {
-//            File file = new File(filePath);
-//            if (filePath.endsWith("mp4")) {
-//                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-//                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "video.mp4", requestBody));
-//                Http.request(new ResponseCallback<UploadRsp>() {
-//                    @Override
-//                    protected void onResponse(UploadRsp rsp) {
-//                        find.setUrl(positon, rsp.shortUrl);
-//                        submitSociety(find);
-//                    }
-//                }, verifyCodeLoginObservable);
-//            } else {
-//                Luban.with(ExpoApp.getApplication())
-//                        .load(filePath)
-//                        .ignoreBy(100)
-//                        .setTargetDir(Constants.Config.TEMP_PATH)
-//                        .filter(new CompressionPredicate() {
-//                            @Override
-//                            public boolean apply(String path) {
-//                                return !(TextUtils.isEmpty(path) || path.toLowerCase().endsWith(".gif"));
-//                            }
-//                        })
-//                        .setCompressListener(new OnCompressListener() {
-//                            @Override
-//                            public void onStart() {
-//                                // TODO 压缩开始前调用，可以在方法内启动 loading UI
-//                            }
-//
-//                            @Override
-//                            public void onSuccess(File file) {
-//                                // TODO 压缩成功后调用，返回压缩后的图片文件
-//                                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-//                                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "image.png", requestBody));
-//                                Http.request(new ResponseCallback<UploadRsp>() {
-//                                    @Override
-//                                    protected void onResponse(UploadRsp rsp) {
-//                                        find.setUrl(positon, rsp.shortUrl);
-//                                        submitSociety(find);
-//                                    }
-//
-//                                    @Override
-//                                    public void onComplete() {
-//                                        file.deleteOnExit();
-//                                        super.onComplete();
-//                                    }
-//                                }, verifyCodeLoginObservable);
-//                            }
-//
-//                            @Override
-//                            public void onError(Throwable e) {
-//                                // TODO 当压缩过程出现问题时调用
-//                                File file = new File(filePath);
-//                                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-//                                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "image.png", requestBody));
-//                                Http.request(new ResponseCallback<UploadRsp>() {
-//                                    @Override
-//                                    protected void onResponse(UploadRsp rsp) {
-//                                        find.setUrl(positon, rsp.shortUrl);
-//                                        submitSociety(find);
-//                                    }
-//                                }, verifyCodeLoginObservable);
-//                            }
-//                        }).launch();
-//            }
-//        } else {
-//            submitSociety(find);
-//        }
-
         if (!StringUtils.isEmpty(filePath)) {
             File file = new File(filePath);
-            String fileType = "";
-            if(filePath.endsWith(".mp4"))
-                fileType="video.mp4";
-            else
-                fileType="image.png";
-            RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-            Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", fileType, requestBody));
-            Http.request(new ResponseCallback<UploadRsp>() {
-                @Override
-                protected void onResponse(UploadRsp rsp) {
-                    find.setUrl(positon, rsp.shortUrl);
-                    submitSociety(find);
-                }
-            }, verifyCodeLoginObservable);
+            if (filePath.endsWith("mp4")) {
+                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "video.mp4", requestBody));
+                Http.request(new ResponseCallback<UploadRsp>() {
+                    @Override
+                    protected void onResponse(UploadRsp rsp) {
+                        find.setUrl(positon, rsp.shortUrl);
+                        submitSociety(find);
+                    }
+                }, verifyCodeLoginObservable);
+            } else {
+                Luban.with(ExpoApp.getApplication())
+                        .load(filePath)
+                        .ignoreBy(200)
+                        .setTargetDir(new File(filePath).getParentFile().getPath())
+                        .filter(new CompressionPredicate() {
+                            @Override
+                            public boolean apply(String path) {
+                                return !(TextUtils.isEmpty(path) || path.toLowerCase().endsWith(".gif"));
+                            }
+                        })
+                        .setRenameListener(new OnRenameListener() {
+                            @Override
+                            public String rename(String filePath) {
+                                String newName = TimeUtils.getNowMills() + ".jpg";
+                                filePath = new File(filePath).getParent() + File.separator + newName;
+                                FileUtils.createFile(filePath);
+
+                                return newName;
+                            }
+                        })
+                        .setCompressListener(new OnCompressListener() {
+                            @Override
+                            public void onStart() {
+                                // TODO 压缩开始前调用，可以在方法内启动 loading UI
+                            }
+
+                            @Override
+                            public void onSuccess(File file) {
+                                // TODO 压缩成功后调用，返回压缩后的图片文件
+                                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "image.png", requestBody));
+                                Http.request(new ResponseCallback<UploadRsp>() {
+                                    @Override
+                                    protected void onResponse(UploadRsp rsp) {
+                                        find.setUrl(positon, rsp.shortUrl);
+                                        submitSociety(find);
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+                                        if (!StringUtils.equals(file.getPath(), filePath))
+                                            file.delete();
+                                        super.onComplete();
+                                    }
+                                }, verifyCodeLoginObservable);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                // TODO 当压缩过程出现问题时调用
+                                File file = new File(filePath);
+                                RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                                Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", "image.png", requestBody));
+                                Http.request(new ResponseCallback<UploadRsp>() {
+                                    @Override
+                                    protected void onResponse(UploadRsp rsp) {
+                                        find.setUrl(positon, rsp.shortUrl);
+                                        submitSociety(find);
+                                    }
+
+                                }, verifyCodeLoginObservable);
+                            }
+                        }).launch();
+            }
         } else {
             submitSociety(find);
         }
+
+//        if (!StringUtils.isEmpty(filePath)) {
+//            File file = new File(filePath);
+//            String fileType = "";
+//            if(filePath.endsWith(".mp4"))
+//                fileType="video.mp4";
+//            else
+//                fileType="image.png";
+//            RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+//            Observable<UploadRsp> verifyCodeLoginObservable = Http.getServer().uploadFile(MultipartBody.Part.createFormData("file", fileType, requestBody));
+//            Http.request(new ResponseCallback<UploadRsp>() {
+//                @Override
+//                protected void onResponse(UploadRsp rsp) {
+//                    find.setUrl(positon, rsp.shortUrl);
+//                    submitSociety(find);
+//                }
+//            }, verifyCodeLoginObservable);
+//        } else {
+//            submitSociety(find);
+//        }
     }
 
     private synchronized void submitSociety(Find find) {
