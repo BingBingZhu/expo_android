0233cd6c9787c72120dfb3bd7a2dba446e1afff6
william <aitadestone@sohu.com>
2019-01-14 09:22:17 +0800

    精简代码 本地提交 1

--- app/src/main/assets/newWebAr/js/testInfo.js
+++
@@ -1,5 +0,0 @@
-// webview 测试数据输出
-function testInfo(info){
-    var test = document.getElementById('testInfo');
-    test.innerHTML = JSON.stringify(info);
-}
--- app/src/main/java/com/expo/module/share/ShareUtil.java
+++ app/src/main/java/com/expo/module/share/ShareUtil.java
@@ -337,36 +337,6 @@ public class ShareUtil {
         mShareParams.put( "isShareTencentWeibo", true );
     }
 
-    public static void showCustomShare(Context context, String title, String text, String imageUrl, String shareUrl, PlatformActionListener listener) {
-        ShareUtil mShareUtil = new ShareUtil((Activity) context);
-        mShareUtil.setPlatformActionListener(listener);
-        Dialog dialog = new Dialog(context, R.style.BottomActionSheetDialogStyle);
-        View v = LayoutInflater.from(context).inflate(R.layout.dialog_layout_share, null);
-        ImageView imgWechat = v.findViewById(R.id.share_wechat);
-        ImageView imgQQ = v.findViewById(R.id.share_qq);
-        ImageView imgSina = v.findViewById(R.id.share_sina);
-        ImageView imgWechatMoments = v.findViewById(R.id.share_wechat_moments);
-        ImageView imgQzone = v.findViewById(R.id.share_qzone);
-        TextView tvCancel = v.findViewById(R.id.share_cancel);
-        imgWechat.setOnClickListener(v1 -> { mShareUtil.doShare( Wechat.NAME, title, text, imageUrl, shareUrl ); dialog.dismiss(); });
-        imgQQ.setOnClickListener(v1 -> { mShareUtil.doShare( QQ.NAME, title, text, imageUrl, shareUrl ); dialog.dismiss(); });
-        imgSina.setOnClickListener(v1 -> { mShareUtil.doShare( SinaWeibo.NAME, title, text, imageUrl, shareUrl ); dialog.dismiss(); });
-        imgQzone.setOnClickListener(v1 -> { mShareUtil.doShare( WechatMoments.NAME, title, text, imageUrl, shareUrl ); dialog.dismiss(); });
-        imgWechatMoments.setOnClickListener(v1 -> { mShareUtil.doShare( QZone.NAME, title, text, imageUrl, shareUrl ); dialog.dismiss(); });
-        tvCancel.setOnClickListener(v1 -> dialog.dismiss() );
-        dialog.setContentView(v);
-        Window dialogWindow = dialog.getWindow();
-        if(dialogWindow == null){
-            return;
-        }
-        dialogWindow.setGravity(Gravity.BOTTOM);
-        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-        lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
-//        lp.y = 20;//设置Dialog距离底部的距离
-        dialogWindow.setAttributes(lp);
-        dialog.show();//显示对话框
-    }
-
     public static void showShare(Context context, String title, String text, String imageUrl, String shareUrl, PlatformActionListener listener) {
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
